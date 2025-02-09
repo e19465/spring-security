c49abbe3f4c9a59a3a8e18ee0a519098d39412dd
Sasindu Dilshan Rathnayaka <sasindudil0002@gmail.com>
2025-02-09 19:26:12 +0530

    custom CORS config

--- src/main/java/com/sasindu/springsecurity/security/config/SecurityConfig.java
+++ src/main/java/com/sasindu/springsecurity/security/config/SecurityConfig.java
@@ -49,6 +49,11 @@ public class SecurityConfig {
     }
 
 
+    /**
+     * CorsConfigurationSource bean - setting the CORS configuration
+     *
+     * @return CorsConfigurationSource object
+     */
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
